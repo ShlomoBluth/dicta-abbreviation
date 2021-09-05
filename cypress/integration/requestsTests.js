9619fa6367dfca43cb3cb32007fe7916a490841a
Shlomo Bluth <lomibluth@gmail.com>
2021-09-05 10:00:06 +0300

    add commit

--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -13,61 +13,56 @@ sizes.set('desktop',[1000, 660])
 
 urls.forEach((urlValue,urlKey)=>{
 
-    sizes.forEach((sizeValue,sizeKey) => {
+  sizes.forEach((sizeValue,sizeKey) => {
 
 
-      describe('requestsTests',()=>{
+    describe('requestsTests '+urlKey+' '+sizeKey,()=>{
 
     
-        beforeEach(() => {
-          cy.screenSize({size:size})
-          cy.visitpage({url:'https://abbreviation.dicta.org.il/'})
-        })
-      
-        
-      
-      
-        it('Error message for api response with a delay of 2 minutes when clicking the run button'+
-        ' of abbreviation page',()=>{
-          cy.abbreviationRequest({
-            url:'/api',
-            message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-            delaySeconds:60*2
-          })
-        })
-      
-        
-        it('Error message for api response with status code 500 when clicking the run button of abbreviation page'
-        ,()=>{
-          cy.abbreviationRequest({
-            url:'/api',
-            status:500,
-            message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
-          })
-        })
-      
-        it('Error message for genreclassify response with a delay of 2 minutes when clicking the run button'+
-        ' of abbreviation page',()=>{
-          cy.abbreviationRequest({
-            url:'/genreclassify',
-            message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
-            delaySeconds:60*2
-          })
-        })
-      
-        
-        it('Error message for genreclassify response with status code 500 when clicking the run button of abbreviation page'
-        ,()=>{
-          cy.abbreviationRequest({
-            url:'/genreclassify',
-            status:500,
-            message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
-          })
-        })
-          
+      beforeEach(() => {
+        cy.screenSize({size:sizeValue})
+        cy.visitpage({url:urlValue})
       })
-    
-    })
+  
+      it('Error message for api response with a delay of 2 minutes when clicking the run button'+
+      ' of abbreviation page',()=>{
+        cy.abbreviationRequest({
+          url:'/api',
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+          delaySeconds:60*2
+        })
+      })
+  
+      it('Error message for api response with status code 500 when clicking the run button of abbreviation page'
+      ,()=>{
+        cy.abbreviationRequest({
+          url:'/api',
+          status:500,
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+        })
+      })
+        
+      it('Error message for genreclassify response with a delay of 2 minutes when clicking the run button'+
+      ' of abbreviation page',()=>{
+        cy.abbreviationRequest({
+          url:'/genreclassify',
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר',
+          delaySeconds:60*2
+        })
+      })      
+          
+      it('Error message for genreclassify response with status code 500 when clicking the run button of abbreviation page'
+      ,()=>{
+        cy.abbreviationRequest({
+          url:'/genreclassify',
+          status:500,
+          message:'לא ניתן לגשת כעת לשרת, נסה שוב מאוחר יותר'
+        })
+      })
+      
+             
+    })      
   })
+})
 
 
